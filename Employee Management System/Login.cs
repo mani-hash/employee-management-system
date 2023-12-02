2a198cd6bd67526ef3c81f8ad02b527ef4c868e2
mani-hash <manimehalan400@gmail.com>
2023-12-02 19:19:01 +0530

    created login functionality

--- Employee Management System/Login.cs
+++ Employee Management System/Login.cs
@@ -13,12 +13,89 @@ namespace Employee_Management_System
 {
     public partial class Login : Form
     {
+        private string TxtUsername
+        {
+            get { return Username.Text; }
+        }
+
+        private string TxtPassword
+        { 
+            get { return Password.Text; }
+        }
+
         public Login()
         {
             InitializeComponent();
         }
 
-        //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-IQL9INS;Initial Catalog=ems;Integrated Security=True");
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            Username.Clear();
+            Password.Clear();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            string exitMsg = "Are you sure, Do you really want to exit?";
+            string exitCaption = "Exit";
+
+            DialogResult result = MessageBox.Show(exitMsg, exitCaption, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void DisplayRegistrationForm()
+        {
+            this.Hide();
+            ManageEmployee registration = new ManageEmployee();
+            registration.Show();
+        }
+
+        private static void InvalidLogin()
+        {
+            string errorMsg = "Invalid Login Credentials! Please check your username/password and try again..";
+            string errorCaption = "Invalid Login";
+            MessageBox.Show(errorMsg, errorCaption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            string? connectionString = Database.GetConnectionString();
+            string query = $@"SELECT * FROM Login WHERE username = @UsernameValue AND password = @PasswordValue";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+
+                    SqlCommand sqlCommand = new SqlCommand(query, con);
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@UsernameValue", this.TxtUsername));
+                    sqlCommand.Parameters.Add(new SqlParameter("@PasswordValue", this.TxtPassword));
+
+                    SqlDataReader row = sqlCommand.ExecuteReader();
+
+                    if (row.HasRows)
+                    {
+                        this.DisplayRegistrationForm();
+                    }
+                    else
+                    {
+                        InvalidLogin();
+                    }
+
+                } 
+                catch (Exception ex)
+                {
+                    string errorMsg = "An error occured";
+                    MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
 
 
 
