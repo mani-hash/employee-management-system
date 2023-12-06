157158cce72bad5265aedb61ddd02db4461f67e6
mani-hash <manimehalan400@gmail.com>
2023-12-06 20:23:51 +0530

    created mainform for common methods

---
+++ Employee Management System/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System
+{
+    public class MainForm: Form
+    {
+        protected readonly string? connectionString;
+
+        public MainForm()
+        {
+            this.connectionString = Database.GetConnectionString();
+        }
+
+        protected static void ShowConnectError()
+        {
+            string errorMsg = "An error occured!";
+            MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        protected static void ShowExitApp()
+        {
+            string exitMsg = "Are you sure, Do you really want to exit?";
+            string exitCaption = "Exit";
+
+            DialogResult result = MessageBox.Show(exitMsg, exitCaption, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
